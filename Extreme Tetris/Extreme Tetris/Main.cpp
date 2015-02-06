b10a0dca06be904afe83e70f3d7fb104b16ec226
Niko Kinnunen <nikokinnunen92@gmail.com>
2015-02-06 10:53:46 +0200

    lisätty kuva

--- Extreme Tetris/Extreme Tetris/Main.cpp
+++ Extreme Tetris/Extreme Tetris/Main.cpp
@@ -2,27 +2,32 @@
 #include <SFML/Graphics/Texture.hpp>
 #include <SFML/Graphics/Sprite.hpp>
 #include <SFML/Window/Event.hpp>
+#include <iostream>
 
 
 using namespace sf;
 
 int main()
 {
-	sf::Texture texture;
-	sf::Sprite sprite;
-	if (!texture.loadFromFile("C:\\Users\ttv14slaurih\Documents\GitHub\ExtremeTetris\Graphics\Block.png"))
+	RenderWindow window(VideoMode(400u, 400u), "SFML Demo");
+	
+	
+	Texture texture;
+	Sprite sprite;
+
+	if (!texture.loadFromFile("..\\Graphics\\Block.png"))
 	{
-		// error...
+		std::cout << "Error loading block texture" << std::endl;
 	}
+	
+
 	texture.setSmooth(true);
 	sprite.setTexture(texture);
 
-	RenderWindow window(VideoMode(200u, 200u), "SFML Demo");
-	Event event;
-	window.draw(sprite);
 
 	while (window.isOpen())
 	{
+		Event event;
 		while (window.pollEvent(event))
 		{
 			if (event.type == Event::Closed)
@@ -30,6 +35,10 @@ int main()
 				window.close();
 			}
 		}
+
+		window.clear();
+		window.draw(sprite);
+		window.display();
 	}
 	return 0;
 }
